8e85ff4d7f46c0dd1205b5446bf9ad21d8011d53
Thulasi Ram <thulasi503@gmail.com>
2022-04-19 12:02:28 +0530

    test: added list supplier test

--- purchases/controllers/supplier_test.go
+++ purchases/controllers/supplier_test.go
@@ -101,4 +101,42 @@ func TestGetSupplierEndpoint(t *testing.T) {
 		assert.Equal(t, supplierResponse.Data.Address, payload["address"])
 
 	})
+	t.Run("test list suppliers", func(t *testing.T) {
+		app := test.SetupTest()
+
+		repo := repository.New(app.DB())
+		supplier, err := repo.CreateSupplier(context.Background(), repository.CreateSupplierParams{
+			Name:    "TestSupplier",
+			Phone:   "xxxxxxxx",
+			Address: "test address 1",
+		})
+
+		req, err := http.NewRequest("GET", "/api/suppliers/", nil)
+		require.Nil(t, err)
+		req.Header.Set("Content-Type", "application/json")
+
+		response := testSupplierServer(app, req)
+
+		assert.Equal(t, http.StatusOK, response.Code)
+
+		supplierResponse := struct {
+			Data []struct {
+				ID      int64  `json:"ID"`
+				Name    string `json:"Name"`
+				Phone   string
+				Address string
+			} `json:"data"`
+		}{}
+		supplierResponse2 := map[string]interface{}{}
+		err = json.Unmarshal(response.Body.Bytes(), &supplierResponse)
+		err = json.Unmarshal(response.Body.Bytes(), &supplierResponse2)
+		assert.Nil(t, err)
+		assert.Equal(t, len(supplierResponse.Data), 1)
+
+		assert.Equal(t, supplierResponse.Data[0].ID, supplier.ID)
+		assert.Equal(t, supplierResponse.Data[0].Name, supplier.Name)
+		assert.Equal(t, supplierResponse.Data[0].Phone, supplier.Phone)
+		assert.Equal(t, supplierResponse.Data[0].Address, supplier.Address)
+
+	})
 }
